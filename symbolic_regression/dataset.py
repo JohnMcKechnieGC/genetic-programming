ac554da1130ff3208c4d3cc57113eecd5d24649b
jmmckechnie <jmmckechnie@gmail.com>
2023-12-14 15:21:33 +0000

    Fix exponent in test function

--- symbolic_regression/dataset.py
+++ symbolic_regression/dataset.py
@@ -5,6 +5,6 @@ def get_data_points(n=100):
     data_points = []
     for _ in range(n):
         x = (random() - 0.5) * 2  # -0.5 (inc) to 0.5 (ex)
-        y = x ** 5 + x ** 4 + x ** 3 + x ** 1 + x
+        y = x ** 5 + x ** 4 + x ** 3 + x ** 2 + x
         data_points.append((x, y))
     return data_points
